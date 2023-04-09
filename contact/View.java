000fc69d863c7dbcc8ed91384aca03aa47f35b88
uday0432 <udayprakashathaghara@gmail.com>
2023-04-10 02:21:51 +0530

    view class updated just now.

--- contact/View.java
+++ contact/View.java
@@ -1,5 +1,21 @@
 package contact;
+//import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
 
-public class View {
+public class View extends JFrame {
+	
+	JLable heading;
+	public View(){
+		new heading("View Contacts");
+		heading.setFont(new Font("Helvetica",Font.Italic,72));
+		heading.setBounds(300,50,80,40);
+		heading.setForeground(color.Black));
+
+		getContentsPane().setBackground(Color.ORANGE);
+		setSize(1000, 500);
+		setVisible(true);
+		setLocation(300,50);
+	}
 
 }
