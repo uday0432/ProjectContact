b231bab49f4a22168be9451230f92679ab35874a
Subham Das <das187213@gmail.com>
2023-04-12 02:53:47 +0530

    Finally Done

---
+++ contact/CreateExcel.java
@@ -0,0 +1,45 @@
+package contact;
+
+import jxl.Workbook;
+import jxl.write.*;
+import jxl.write.biff.RowsExceededException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateExcel {
+
+	String excelname = new String("Contacts.xls");
+	
+	WritableWorkbook workbook;
+	WritableCell[] cell;
+	WritableSheet sheet;
+	
+	void defaultValue() throws RowsExceededException, WriteException {
+		WritableCell cell1 = new Label(0,0,"Name");
+		WritableCell cell2 = new Label(1,0,"Number");
+		
+		sheet.addCell(cell1);
+		sheet.addCell(cell2);
+		
+	}
+	CreateExcel() throws ClassNotFoundException, WriteException{
+		try {
+			//It will create a .xls file in current project named Contacts
+			workbook = Workbook.createWorkbook(new File(excelname));
+			sheet = workbook.createSheet("Sheet 1",0);// Sheet Created
+			
+			
+			
+			workbook.write();
+			workbook.close();
+		} catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, RowsExceededException, WriteException{
+		new CreateExcel() ;
+	}
+
+}
