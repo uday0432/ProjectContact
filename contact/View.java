d923511a6dd1f6323cf770a355e66dc9458facef
uday0432 <udayprakashathaghara@gmail.com>
2023-04-12 21:12:54 +0530

    rakesh ka nunu

--- contact/View.java
+++ contact/View.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 
 public class View extends JFrame {
 	View(){
-		System.out.println("Uday Ra bando");
+		System.out.println("Uday Ra bando and subham ro gandi");
 	}
 	public static void main(String[] args) {
 		new View();
